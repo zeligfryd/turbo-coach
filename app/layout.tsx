6b9c6cb7f27553999c1a687e34fa9254e04db233
zeligfryd <41440872+zeligfryd@users.noreply.github.com>
2026-03-28 06:48:33 +0100

    chore: favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -11,6 +11,9 @@ export const metadata: Metadata = {
   metadataBase: new URL(defaultUrl),
   title: "Turbo Coach",
   description: "Turbo Coach Application",
+  icons: {
+    icon: "data:image/svg+xml,<svg xmlns='http://www.w3.org/2000/svg' viewBox='0 0 100 100'><text y='.9em' font-size='90'>🌪️</text></svg>",
+  },
 };
 
 export const viewport: Viewport = {
